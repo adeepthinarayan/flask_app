6c36567cb4116c829ba993ed36d6df92991455d1
adeepthinarayan <112624671+adeepthinarayan@users.noreply.github.com>
2023-08-11 15:30:00 +0530

    Update app.py comments

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# My flask application
-from flask import Flask, render_template, request, redirect
+# My flask application again
+from flask import Flask,  ender_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
